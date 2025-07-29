013a066440aa0aafccb3b69e4d87be67218eef18
Srivalli-2400032712 <2400032712@kluniversity.com>
2025-07-29 15:40:24 +0530

    version1

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Page1 from './Page1.jsx'
 import Page2 from './Page2.jsx'
 import Page3 from './Page3.jsx'
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+<BrowserRouter basename="frontend3">
 <Routes>
   <Route path="/"element={<App/>}></Route>
   <Route path='/Page1'element={<Page1/>}></Route>
